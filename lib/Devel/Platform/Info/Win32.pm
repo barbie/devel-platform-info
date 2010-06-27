79f43e81c72b6f3d22941321c69ba35fa2a2ebfa
Colin Newell <colin.newell@gmail.com>
2010-06-27 22:19:16 +0100

    Added pod

--- lib/Devel/Platform/Info/Win32.pm
+++ lib/Devel/Platform/Info/Win32.pm
@@ -121,3 +121,82 @@ sub InterpretWin32Info
 1;
 
 __END__
+
+=head1 NAME
+
+Devel::Platform::Info::Win32 - Retrieve common platform metadata
+
+=head1 SYNOPSIS
+
+  use Devel::Platform::Info::Win32;
+  my $info = Devel::Platform::Info::Win32->new();
+  my $data = $info->get_info();
+
+=head1 DESCRIPTION
+
+This module is a driver to determine platform metadata regarding the Win32
+operating system. It should be called indirectly via it's parent 
+Devel::Platform::Info
+
+=head1 INTERFACE
+
+=head2 The Constructor
+
+=over
+
+=item * new
+
+Simply constructs the object.
+
+=back
+
+=head2 Methods
+
+=over 4
+
+=item * get_info
+
+Returns a hash reference to the Win32 platform metadata.
+
+Returns the following keys:
+
+  source
+  archname
+  osname
+  osvers
+  oslabel
+  is32bit
+  is64bit
+  osflag
+
+
+=back
+
+=head1 BUGS, PATCHES & FIXES
+
+The module cannot accurately tell the difference between the Windows Server 2003 and Windows XP 64 or Windows Server 2003 R2.
+
+It would be useful if we could tell is we are runing WOW64 but we don't do that yet either.
+
+If you spot a
+bug or are experiencing difficulties, that is not explained within the POD
+documentation, please send bug reports and patches to the RT Queue (see below).
+
+RT Queue -
+http://rt.cpan.org/Public/Dist/Display.html?Name=Devel-Platform-Info
+
+=head1 AUTHORS
+
+  Barbie (BARBIE) <barbie@cpan.org>
+  Brian McCauley (NOBULL) <nobull67@gmail.com>
+  Colin Newell http://colinnewell.wordpress.com/
+  Jon 'JJ' Allen (JONALLEN) <jj@jonallen.info>
+
+=head1 COPYRIGHT & LICENSE
+
+  Copyright (C) 2010 Birmingham Perl Mongers
+
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic License 2.0.
+
+=cut
